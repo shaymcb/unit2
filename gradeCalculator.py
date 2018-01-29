2437a9119cc9b2d2e096a7115b5f9726d2c744c1
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-29 08:25:22 -0500

    Updated from Brython Server: 1/29/2018 8:25:21 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,16 @@
+#Shaylee McBride
+#29Jan2018
+#gradeCalculator.py - input grade and it gives you letter
 
+grade = float(input('Input your grade: '))
+
+if grade > 90:
+    print('You got an A. Whoop-dee-doo.')
+elif grade > 80:
+    print("You got a B. That's not the worst.")
+elif grade > 70:
+    print("You got a C. You suck at life.")
+elif grade > 60:
+    print("You got a D. Might as well quit school.")
+else:
+    print("You failed. Nice!")
