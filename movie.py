c891c86b3e873e74e3b715caaf572505a740b72a
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-29 08:34:13 -0500

    Updated from Brython Server: 1/29/2018 8:34:12 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,15 @@
+#Shaylee McBride
+#29Jan2018
+#movie.py - most scandalous movie you can watch
 
+age = int(input('How old are you? '))
+
+if age < 5:
+    print("You are a child and probably can't even read this. Go watch a G-rated movie.")
+elif age < 13:
+    print("You can watch PG movies. Or PG-13 if you have nice parents who'll go with you.")
+elif age < 17:
+    print("You can watch PG-13 movies. Or R if your parents come with you.")
+    print("But who wants that when you can stay home and watch porn online?")
+else:
+    print("You're old. Go watch whatever you want. Nobody cares about you anymore.")
