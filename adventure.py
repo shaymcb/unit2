b0b87c955f37f22377cceabbd0d09ce4d8b0517b
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-30 08:49:30 -0500

    Updated from Brython Server: 1/30/2018 8:49:29 AM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,15 @@
+#Shaylee McBride
+#30Jan2018
+#adventure.py - choose your own adventure
+
+print('You are bored in french class.')
+print('You hear something whack into a tree out the window behind you.')
+print('You look out and think you see a weird green light for a second before it disappears.')
+choice1 = input('Did you imagine it? ')
+if choice1 == 'yes':
+    print('You decide it was just your mind playing tricks on you. You go back to learning le subjonctif and live a boring life.')
+else:
+    print('You look closer and see a strange metal spike sticking out of the woods where you thought you saw the light.')
+    print('The top of the spike lights up. Cool!')
+    print('Suddenly your 
 
