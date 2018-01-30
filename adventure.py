50bbb987bbf6c7dc30688cd868b247ebfa6484f0
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-30 11:54:53 -0500

    Updated from Brython Server: 1/30/2018 11:54:52 AM

--- adventure.py
+++ adventure.py
@@ -9,18 +9,18 @@ choice1 = input('Did you imagine it? ')
 if choice1 == 'yes':
     print('You decide it was just your mind playing tricks on you. You go back to learning le subjonctif and live a boring life.')
     print("THE END")
-else:
+if choice1 == 'no':
     print('You look closer and see a huge metal spike emerging out of the woods where you thought you saw the light.')
     print('The top of the spike lights up an ominous red. This is so cool!')
     print('Suddenly your ears fill with a high-pitched ringing noise. You look around-- everyone else is continuing to discuss the reign of Napoleon III as if nothing is wrong.') 
     print("The sound grows and grows until it is excruciatingly loud. You can't form thoughts. You fall to the ground, clutching your ears and screaming.")
     print("How has nobody noticed you're having a fit? It's not like you're being quiet.")
-    choice2 = input("Do you: (a) Try harder to get someone's attention, (b) Wait it out (c) Walk the the nurse's (d) Jump out the window)
+    choice2 = input("Do you: (a) Try harder to get someone's attention, (b) Wait it out (c) Walk to the nurse's (d) Jump out the window ")
     if choice2 == 'a':
         print("Still lying on the ground, you whack the leg of the person sitting next to you.")
         print("'Claire!' you yell, 'get help!'")
         print("She rubs her leg and looks confused. She doesn't see or hear you. No one does. You're gonna need to do something more drastic.")
-        choice3 = input("Do you (a) Shake Claire until she realizes something weird is going on, or (b) Give up and try to get out of there")
+        choice3 = input("Do you (a) Shake Claire until she realizes something weird is going on, or (b) Give up and try to get out of there? ")
         if choice3 == 'b':
             choice2 = 'd'
         else:
@@ -28,6 +28,9 @@ else:
             print("Everyone thinks Claire is having a seizure. They call 911.")
             print("When the paremedics arrive, you try to get their attention, but they don't notice you.")
             choice2 = 'b'
+    elif choice2 == 'c':
+        print("Your legs feel like jelly. You can't walk. You try to creep toward the door but you can't make it.")
+        choice2 == 'b'
     if choice2 == 'b':
         print("Your head feels like it's splitting open. You pass out and never wake back up.")
         print("THE END")
