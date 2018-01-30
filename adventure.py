0393876af4b7e4013b90cd9d7384ba7e914be5f7
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-30 16:32:27 -0500

    Updated from Brython Server: 1/30/2018 4:32:26 PM

--- adventure.py
+++ adventure.py
@@ -9,7 +9,7 @@ if choice1 == 'yes':
     print("THE END")
 if choice1 == 'no':
     print('You look closer and see a huge metal spike emerging out of the woods where you thought you saw the light. The top of the spike lights up an ominous red. Suddenly your ears fill with a high-pitched ringing noise.') 
-    print("The sound becomes excruciatingly loud. You fall to the ground, clutching your ears and screaming. How has nobody noticed this?")
+    print("The sound becomes excruciatingly loud. You fall to the ground, clutching your ears and screaming. How has nobody noticed you're freaking out?")
     choice2 = input("Do you: (a) Try to get someone's attention, (b) Wait it out (c) Walk to the nurse's (d) Jump out the window towards the mysterious spike ")
     if choice2 == 'a':
         print("Still lying on the ground, you whack the leg of the person sitting next to you.")
@@ -30,10 +30,16 @@ if choice1 == 'no':
         print("Your head feels like it's splitting open. You pass out and never wake back up.")
         print("THE END")
     if choice2 == 'd':
-        print("As soon as you get the window open, the noise stops. A wispy tendril reaches out from the spike pulls you in.")
+        print("As soon as you get the window open, the noise stops. A wispy tendril reaches out from the spike and pulls you in.")
         print("You find yourself in a round metal windowless room full of a clear jelly. You sink into the jelly-- you cannot breathe and struggle to get out.")
         print("Suddenly all the jelly recedes. You watch in horror as it takes shape and gains color, forming a perfect copy of you.")
         print("'What's going on?' you scream. 'What's going on?' not-you screams back in your voice. Then it runs unsteadily out a hatch and into the school.")
         print("The hatch closes behind it, leaving you trapped. 'What the fuck', you say. What do you do now?")
-        choice3 = input("(a) Try to break open the hatch and escape, (b) Call for help, or (c) Wait for not-you to return ")
-
+        choice4 = input("(a) Try to break open the hatch and escape, (b) Call for help, or (c) Wait for not-you to return ")
+        if choice4 == 'b':
+            print("Somehow you have reception. Whatever the blob was, it didn't plan on cell phones.")
+            choice5 = input('Do you call (a) your friend, (b) your mom, or (c) 9-1-1? ')
+            if choice5 == 'a':
+                print("Your friend picks up on one ring. It's so nice to have people you can count on. 'Tish!' you say, 'I need help.'")
+                print("'Shouldn't you be in French?' she asks 'I'm not helping you cheat.'")
+                print("'No, it's not that' you say. 'I think I've been body-snatched.'")
