aeb3110b6111254f9b27867f52af0a2b95c98d46
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-30 08:28:57 -0500

    Update fortuneTeller.py

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,29 @@
+#Shaylee McBride
+#30Jan2018
+#fortuneTeller.py - tells yo fortune
 
+color = input('red pill or blue pill? ')
+num = int(input('pick a number between 1 and 4: ')
+
+if color == 'red':
+    if num == 1:
+        print('you will die alone, having achieved nothing and lost everything')
+    elif num == 2:
+        print('beware of the man with the crooked tooth')
+    elif num == 3:
+        print('tomorrow you will catch the flu and dry-drown')
+    else:
+        print("everyone you've ever loved will leave you in the end")
+elif color == 'blue':
+    if num == 1:
+        print('you were an accident')
+    elif num == 2:
+        print('the one with the power to vanquish the dark lord approaches...')
+        print('born to those who have thrice defied him, born as the seventh month dies...')
+        print('and the dark lord will mark him as his equal, but he will have power the dark lord knows not...')
+        print('and either must die at the hand of the other, for neither can live while the other survives...')
+        print('the one with the power to vanquish the dark lord will be born as the seventh month dies...')
+    elif num == 3:
+        print('you will be betrayed when you need help the most by the one you trust most')
+    else:
+        print('your dead body will be discovered by your toddler, who will be traumatized forever')
