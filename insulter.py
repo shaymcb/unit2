a64736bec433f2b8226364aa65a1131d0b8056f0
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-01-30 08:38:27 -0500

    Updated from Brython Server: 1/30/2018 8:38:27 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,19 @@
+#Shaylee McBride
+#30Jan2018
+#insulter.py - insults you
 
+from random import randint
+
+input('What is your name? ')
+rand = randint(1,5)
+
+if rand == 1:
+    print('are you sleeping ok? you look like shit today')
+if rand == 2:
+    print("your mom doesn't love you")
+if rand == 3:
+    print("if i had a dollar for every brain cell you had, i'd have one dollar.")
+if rand == 4:
+    print("you're a directionless deadbeat who's heading off to college because it's what everyone else does but you know you can't cut it and you're not willing to put in the work and one day it's all gonna catch up with you and no one will be surprised.")
+if rand == 5:
+    print('wow what a douchey name')
