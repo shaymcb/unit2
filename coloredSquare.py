c7a8ca08088101698427021f32f6c97fbe7893ee
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-02 08:33:49 -0500

    Updated from Brython Server: 2/2/2018 8:33:48 AM

--- coloredSquare.py
+++ coloredSquare.py
@@ -6,11 +6,20 @@ from ggame import *
 from random import randint
 
 rand = randint(1,4)
+size = randint(100,500)
 
-red = Color(0xff0000, 1)
-line = LineStyle(3,red)
-rectangle = RectangleAsset(100,100,line,red)
+if rand == 1:
+    color = Color(0xFF0000, 1)
+if rand == 2:
+    color = Color(0x00FF00, 1)
+if rand == 3:
+    color = Color(0x0000FF, 1)
+if rand == 4:
+    color = Color(0xFFFF00, 1)
 
+line = LineStyle(3,color)
+rectangle = RectangleAsset(size,size,line,color)
+    
 Sprite(rectangle)
 myApp = App()
 myApp.run()
