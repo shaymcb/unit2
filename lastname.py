6a87e9537801b9d6cbf3aadd895722595d222ce3
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-02 08:39:25 -0500

    Updated from Brython Server: 2/2/2018 8:39:24 AM

--- lastname.py
+++ lastname.py
@@ -1,1 +1,10 @@
+#Shaylee McBride
+#2Feb2018
+#lastname.py - which side of the room do you stand on when checking in to events
 
+name = input("Enter your last name: ")
+
+if name > 'M':
+    print(name,'is in the second half of the alphabet.')
+else:
+    print(name,'is in the first half of the alphabet.')
