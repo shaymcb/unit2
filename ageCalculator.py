bddf088a7702df34ae31d0400e931d39d82e7f75
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-02 08:48:06 -0500

    Updated from Brython Server: 2/2/2018 8:48:05 AM

--- ageCalculator.py
+++ ageCalculator.py
@@ -1,1 +1,12 @@
+#Shaylee McBride
+#2Feb2018
+#ageCalculator.py - for those of you that have a panic attack when the airport staff asks how old you are
 
+from datetime import date
+
+year = int(input('Enter the year you were born: ')
+month = int(input('Enter the month you were born: ')
+day = int(input('Enter the day you were born: ')
+
+if month > month.today.month or (month == month.today.month and day > date.today.date)
+    
