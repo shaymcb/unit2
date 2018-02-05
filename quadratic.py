61e762c352659abd66e8740adeb80ffbb3e1fb20
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-05 08:14:23 -0500

    Updated from Brython Server: 2/5/2018 8:14:22 AM

--- quadratic.py
+++ quadratic.py
@@ -1,1 +1,19 @@
+#Shaylee McBride
+#5Feb2018
+#quadratic.py - quadratic formula
 
+from math import sqrt
+
+print('Ex: ax^2 + bx + c')
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+
+discriminant = b**2 - 4*a*c
+
+if discriminant >= 0:
+    print('x =',(-b + sqrt(discriminant))/(2*a),'or')
+    print('x =',(-b - sqrt(discriminant))/(2*a))
+if discriminant < 0:
+    print('x =',-b/(2*a),'+',sqrt(-discriminant)/(2*a),'i or')
+    print('x =',-b/(2*a),'-',sqrt(-discriminant)/(2*a))
