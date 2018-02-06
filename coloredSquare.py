f0be3416c8e2bc2432b077498097f5a9facfc59f
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-05 21:19:49 -0500

    Updated from Brython Server: 2/5/2018 9:19:48 PM

--- coloredSquare.py
+++ coloredSquare.py
@@ -6,9 +6,6 @@ from ggame import *
 from random import randint
 
 rand = randint(0,15)
-n = 0
-
-if n < 6:
 if rand < 10:
     rand = str(rand)
 elif rand == 10:
@@ -23,14 +20,101 @@ elif rand == 14:
     rand = 'E'
 else:
     rand = 'F'
-
 color = '0x'+rand
 
+rand = randint(0,15)
+if rand < 10:
+    rand = str(rand)
+elif rand == 10:
+    rand = 'A'
+elif rand == 11:
+    rand = 'B'
+elif rand == 12:
+    rand = 'C'
+elif rand == 13:
+    rand = 'D'
+elif rand == 14:
+    rand = 'E'
+else:
+    rand = 'F'
+color = color+rand
 
-#size = randint(100,500)
-#line = LineStyle(3,colorLine)
-#rectangle = RectangleAsset(size,size,line,colorSquare)
+rand = randint(0,15)
+if rand < 10:
+    rand = str(rand)
+elif rand == 10:
+    rand = 'A'
+elif rand == 11:
+    rand = 'B'
+elif rand == 12:
+    rand = 'C'
+elif rand == 13:
+    rand = 'D'
+elif rand == 14:
+    rand = 'E'
+else:
+    rand = 'F'
+color = color+rand
+
+rand = randint(0,15)
+if rand < 10:
+    rand = str(rand)
+elif rand == 10:
+    rand = 'A'
+elif rand == 11:
+    rand = 'B'
+elif rand == 12:
+    rand = 'C'
+elif rand == 13:
+    rand = 'D'
+elif rand == 14:
+    rand = 'E'
+else:
+    rand = 'F'
+color = color+rand
+
+rand = randint(0,15)
+if rand < 10:
+    rand = str(rand)
+elif rand == 10:
+    rand = 'A'
+elif rand == 11:
+    rand = 'B'
+elif rand == 12:
+    rand = 'C'
+elif rand == 13:
+    rand = 'D'
+elif rand == 14:
+    rand = 'E'
+else:
+    rand = 'F'
+color = color+rand
+
+rand = randint(0,15)
+if rand < 10:
+    rand = str(rand)
+elif rand == 10:
+    rand = 'A'
+elif rand == 11:
+    rand = 'B'
+elif rand == 12:
+    rand = 'C'
+elif rand == 13:
+    rand = 'D'
+elif rand == 14:
+    rand = 'E'
+else:
+    rand = 'F'
+color = color+rand
+
+print(color)
+
+colorSquare = Color(color, 1)
+
+size = randint(100,500)
+line = LineStyle(3,colorSquare)
+rectangle = RectangleAsset(size,size,line,colorSquare)
     
-#Sprite(rectangle)
-#myApp = App()
-#myApp.run()
+Sprite(rectangle)
+myApp = App()
+myApp.run()
